b55a1e98c0c64c917978f7578c39bf7cae03e272
sancarbar <sancarbar@gmail.com>
2019-03-05 19:47:57 -0500

    Added fab click listener and error message.

--- app/src/main/java/com/platzi/android/firestore/ui/activity/TraderActivity.kt
+++ app/src/main/java/com/platzi/android/firestore/ui/activity/TraderActivity.kt
@@ -2,7 +2,9 @@ package com.platzi.android.firestore.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.platzi.android.firestore.R
+import kotlinx.android.synthetic.main.activity_trader.*
 
 
 /**
@@ -15,5 +17,16 @@ class TraderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trader)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, getString(R.string.generating_new_cryptos), Snackbar.LENGTH_SHORT)
+                .setAction("Info", null).show()
+        }
+
+    }
+
+    fun showGeneralServerErrorMessage() {
+        Snackbar.make(fab, getString(R.string.error_while_connecting_to_the_server), Snackbar.LENGTH_LONG)
+            .setAction("Info", null).show()
     }
 }
