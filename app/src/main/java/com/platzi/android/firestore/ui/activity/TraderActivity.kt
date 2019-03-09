f3557927658588706cdd13eb728d1077f254f273
sancarbar <sancarbar@gmail.com>
2019-03-09 09:36:01 -0500

    Implementación RecyclerView criptomonedas.

--- app/src/main/java/com/platzi/android/firestore/ui/activity/TraderActivity.kt
+++ app/src/main/java/com/platzi/android/firestore/ui/activity/TraderActivity.kt
@@ -2,21 +2,37 @@ package com.platzi.android.firestore.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
 import com.platzi.android.firestore.R
+import com.platzi.android.firestore.adapter.CryptosAdapter
+import com.platzi.android.firestore.adapter.CryptosAdapterListener
+import com.platzi.android.firestore.model.Crypto
+import com.platzi.android.firestore.network.Callback
+import com.platzi.android.firestore.network.FirestoreService
 import kotlinx.android.synthetic.main.activity_trader.*
+import java.lang.Exception
 
 
 /**
  * @author Santiago Carrillo
  * 2/14/19.
  */
-class TraderActivity : AppCompatActivity() {
+class TraderActivity : AppCompatActivity(), CryptosAdapterListener {
+
+    lateinit var firestoreService: FirestoreService
+
+    private val cryptosAdapter: CryptosAdapter = CryptosAdapter(this)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trader)
+        firestoreService = FirestoreService(FirebaseFirestore.getInstance())
+
+        configureRecyclerView()
+        loadCryptos()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, getString(R.string.generating_new_cryptos), Snackbar.LENGTH_SHORT)
@@ -25,8 +41,36 @@ class TraderActivity : AppCompatActivity() {
 
     }
 
+    private fun loadCryptos() {
+        firestoreService.getCryptos(object : Callback<List<Crypto>> {
+            override fun onSuccess(result: List<Crypto>?) {
+                this@TraderActivity.runOnUiThread {
+                    cryptosAdapter.cryptoList = result!!
+                    cryptosAdapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onFailed(exception: Exception) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+    }
+
+    private fun configureRecyclerView() {
+        recyclerView.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = cryptosAdapter
+
+    }
+
     fun showGeneralServerErrorMessage() {
         Snackbar.make(fab, getString(R.string.error_while_connecting_to_the_server), Snackbar.LENGTH_LONG)
             .setAction("Info", null).show()
+    }
+
+    override fun onBuyCryptoClicked(crypto: Crypto) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
